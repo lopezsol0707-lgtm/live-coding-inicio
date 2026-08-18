35a08af2cb4c85eef11ddd3b2121270e9ca64561
lopezsol0707-lgtm <lopez.sol0707@gmail.com>
2026-08-18 09:23:53 -0300

    segunda version

--- live-coding-inicio/script.js
+++ live-coding-inicio/script.js
@@ -10,12 +10,35 @@ const botonSaludo = document.querySelector("#boton-saludo");
 const mensajeSaludo = document.querySelector("#mensaje");
 const miNombre = "Sol";
 const miEdad = 22;
+const botonTema = document.querySelector ("#boton-tema");
+
+const botonLike = document.querySelector ("#boton-like");
 
 
 console.log("Resultado de la búsqueda");
 console.log(mensajeSaludo);
 
+//Agrego funcionalidad al botón saludo
 botonSaludo.addEventListener("click", function() {
     console.log("Apretaste el botón")
-    mensajeSaludo.innerText = "Hola " + miNombre;
+    mensajeSaludo.textContent = "Hola " + miNombre;
+});
+
+botonTema.addEventListener("click", function () {
+    console.log ("Apretaste el botón tema");
+    //Poner y sacar la clase tema-oscuro del body
+    //hacer un toggle de esa clase css sobre un elemento html
+    //Si el body tiene la clase tema-oscuro, la saco, sino la pongo
+    document.body.classList.toggle("tema-oscuro");
+});
+
+//si cr4o la variable con un let la puedo modificar
+let numeroDeLikes = 0;
+
+
+//Agrego funcionalidad al botón like
+botonLike.addEventListener("click", function () {
+    console.log("Apretaste el botón like");
+    numeroDeLikes = numeroDeLikes + 1;
+    botonLike.textContent = "Me gusta ("+ numeroDeLikes +")";
 });
